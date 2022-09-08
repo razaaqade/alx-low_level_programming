df2dbee31b6cc776ab05417aaeb579e73a535e6b
Razaaq <razaaqade@gmail.com>
2022-09-08 13:50:08 -0700

    low level programming

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+C- is my first programming language
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+    int main(void)
+
+{
+
+    return (0);
+
+}
