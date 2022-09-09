c2d5b3d6d7f18be8dbeca2d3747539f87f0ba62d
Razaaq <razaaqade@gmail.com>
2022-09-08 17:15:25 -0700

    low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
-int main(void)
+	int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
