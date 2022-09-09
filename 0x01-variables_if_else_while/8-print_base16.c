0ec2aba0d36401f50611bb292fd992dcd11ca9a0
Razaaq <razaaqade@gmail.com>
2022-09-09 16:10:14 -0700

    c variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int razaaq;
+
+	char ade;
+
+
+	for (razaaq = 48; razaaq < 58; razaaq++)
+
+		putchar(razaaq);
+
+	for (ade = 'a'; ade <= 'f'; ade++)
+
+		putchar(ade);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+		int ade;
+
+
+	for (ade = 48; ade < 58; ade++)
+
+	{
+	putchar(ade);
+
+	if (ade != 57)
+	
+															{
+															putchar(',');
+															putchar(' ');
+															}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
