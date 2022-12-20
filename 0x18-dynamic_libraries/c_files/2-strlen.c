cdf6dddce7366eb2dacc1638c08e8a7a99ea1839
Razaaq <razaaqade@gmail.com>
2022-12-20 03:35:39 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/c_files/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - the length of a string
+ *
+ * @s: pointer to character
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; ++x);
+	return (x);
+}
---
+++ 0x18-dynamic_libraries/c_files/6-abs.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+/**
+ * _abs - return abs value
+ *
+ * @n : number to check
+ *
+ * Return:0 or 1
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
